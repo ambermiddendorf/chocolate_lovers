aa2aad0bd3ecba77b691744c568f7b38b289e78d
swathy84 <93801581+swathy84@users.noreply.github.com>
2022-05-25 06:59:55 +0530

    Adding Schema files

--- assets/Data/schema.sql
+++ assets/Data/schema.sql
@@ -3,10 +3,9 @@ CREATE DATABASE chocolate_db;
 
 --USE chocolate_db;
 
-CREATE TABLE chocolate ( 
-    id INT  NOT NULL,
+CREATE TABLE chocolate( 
+    id INT PRIMARY KEY NOT NULL,
     company_location VARCHAR(250) NOT NULL ,
-    country_of_bean_origin  VARCHAR(50) NOT NULL,
     cocoa_percent int NOT NULL,
     counts_of_ingredients int NOT NULL,
     cocoa_butter boolean NOT NULL,
@@ -14,23 +13,21 @@ CREATE TABLE chocolate (
     lecithin boolean NOT NULL,
     salt boolean NOT NULL,
     sugar boolean NOT NULL,
-    sweetener_without_sugar boolean NOT NULL ,
-    PRIMARY KEY (id)
+    sweetener_without_sugar boolean NOT NULL,
+    ratings int NOT NULL
 );
 
-CREATE TABLE region (
-    id  INT AUTO_INCREMENT NOT NULL,
-    chocolate_id INT NOT NULL,
+CREATE TABLE location (
+    id  SERIAL PRIMARY KEY,
+    country_of_bean_origin  VARCHAR(50) NOT NULL,
     country VARCHAR(50)NOT NULL,
     region VARCHAR(50)NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (chocolate_id) REFERENCES chocolate (id)
+    FOREIGN KEY (country) REFERENCES chocolate (country_of_bean_origin)
 );
 
 CREATE TABLE tastes (
-    index INT  AUTO_INCREMENT NOT NULL,
+    index SERIAL PRIMARY KEY,
     chocolate_id INT NOT NULL,
     word VARCHAR(250) NOT NULL,
-    PRIMARY KEY (index),
     FOREIGN KEY (chocolate_id) REFERENCES chocolate (id)
 );
